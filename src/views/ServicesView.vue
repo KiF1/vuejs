88f58a8a1eda010a33f00242390b73602e7db61e
abner <abner.santos@vegamonitoramento.com.br>
2024-06-10 17:38:13 -0300

    composition api

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Services</h1>
+  <h1>{{ fullName }}</h1>
 </template>
+
+<script>
+import { computed, ref, watch } from 'vue';
+
+export default {
+  // context === Atributos que o componente recebe
+  setup(props , context) {
+    const user = ref({ first_name: 'John', last_name: 'Snow' });
+    const fullName = computed(() => {
+      return `${user.value.first_name} ${user.value.last_name}`
+    }) 
+    
+    watch(
+      // () => user.value.first_name
+      user, () => {
+      console.log('Toda vez que alterar user atualiza')
+    }, { deep: true })
+
+    return { user, fullName }
+  }
+}
+</script>
